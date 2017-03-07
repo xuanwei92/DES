85356921f97f7020cd137e121cb2f8a82111f4cd
xuanwei92 <tanxuanwei92@gmail.com>
2017-03-07 09:27:51 +0800

    Revert "Version1"
    This reverts commit 69ea5eda9f5de514e78cb6cf0a07a40408b28809.

--- FinalYearProject/components/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Footer -->
-<footer class="container-fluid bodyContainer bg-4 text-center">
-    <span class="glyphicon glyphicon-copyright-mark"</span><p class="pFooter">Designed By Tan Xuan Wei, NTU SCSE Final Year Project 2016/2017</p>
-</footer>
